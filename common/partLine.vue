f501fe12785b46227bc3d054c944ed4b1d152c64
lyh2668 <lyh2668@163.com>
2017-10-25 22:01:16 +0800

    first commit

---
+++ common/partLine.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="l-part-line">
+    <div class="text">
+      <span v-text="text">
+        <slot></slot>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LPartLine',
+  props: {
+    text: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.l-part-line {
+  .text {
+    text-align: center;
+    font-size: 13px;
+    display: flex;
+
+    &>span {
+      flex-grow: 1;
+    }
+
+    &::after, &::before {
+      content: "";
+      margin-top: 8px;
+      display: inline-block;
+      height: 0;
+      border-top: 1px solid #cfcfcf;
+      flex-grow: 5;
+    }
+  }
+}
+</style>
